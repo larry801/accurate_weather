9da71ac43971806889113afb90a8a9471bdc5ede
larry801 <larry.wang.801@gmail.com>
2018-09-17 16:06:07 +0800

    use logo provided in email

--- pot.bat
+++ pot.bat
@@ -1,2 +1,2 @@
-scons -c pot&& scons pot && msgmerge -U  addon\locale\zh_CN\LC_MESSAGES\nvda.po accurate_weather.pot
+del accurate_weather.pot && del readme.pot && scons pot && msgmerge -U  addon\locale\zh_CN\LC_MESSAGES\nvda.po accurate_weather.pot && node C:\Users\John\AppData\Roaming\npm\node_modules\gettext-markdown\bin\gettext-md.js -o readme.pot --pot ./addon/doc/en/readme.md
 
